38af422886ccc162b34708852891dc8d7253d2d7
Rylie Fleckenstein <rylie.fleckenstein@trojans.dsu.edu>
2026-07-26 22:28:27 -0500

    updates

--- src/ccd/regression/robust_solver.cpp
+++ src/ccd/regression/robust_solver.cpp
@@ -106,16 +106,10 @@ RobustModel RobustSolver::fit(
             );
         
         scale =
-            std::max(
-                EPS * y_std,
-                mad(resid)
-            );
+            std::max(EPS * y_std, mad(resid));
         
         weights =
-            bisquare(
-                resid / scale,
-                options_.tune
-            );
+            bisquare(resid / scale, options_.tune);
 
         coefficients =
             weighted_fit(
@@ -125,20 +119,139 @@ RobustModel RobustSolver::fit(
                 resid
             );
 
-
         if(converged(old, coefficients))
         {
             break;
         }
-
     }
 
     return RobustModel(
             iter + 1,
             coefficients
         );
- 
 }
+
+//------------------------------------------------------------
+// Weighted least squares
+//------------------------------------------------------------
+Eigen::VectorXd RobustSolver::weighted_fit(
+    const Eigen::MatrixXd& X,
+    const Eigen::VectorXd& y,
+    const Eigen::VectorXd& w,
+    Eigen::VectorXd& residuals
+)
+{
+    Eigen::VectorXd sqrt_w =
+        w.array().sqrt();
+
+    Eigen::MatrixXd Xw =
+        X.array().colwise()
+        * sqrt_w.array();
+
+    Eigen::VectorXd yw =
+        y.array() *
+        sqrt_w.array();
+
+    // Eigen::VectorXd beta =
+    //     Xw.colPivHouseholderQr()
+    //     .solve(yw);
+
+    Eigen::BDCSVD<Eigen::MatrixXd> svd(
+        Xw,
+        Eigen::ComputeThinU |
+        Eigen::ComputeThinV
+    );
+
+    Eigen::VectorXd beta =
+        svd.solve(yw);
+
+    residuals =
+        y - X * beta;
+
+    return beta;
+}
+
+//------------------------------------------------------------
+// Median absolute deviation
+//------------------------------------------------------------
+scalar_t RobustSolver::mad(
+    const Eigen::VectorXd& x
+) const
+{
+    std::vector<scalar_t> values;
+    values.reserve(x.size());
+
+    for(auto v : x)
+        values.push_back(std::abs(v));
+
+    std::sort(
+        values.begin(),
+        values.end()
+    );
+
+    // Python version uses median(rs[4:])
+    // because this is Landsat time-series specific
+    // If you want exact numpy median:
+    // remove this offset.
+    index_t start = 
+        std::min<index_t>(4, values.size());
+
+    std::vector<scalar_t> trimmed(
+        values.begin() + start,
+        values.end()
+    );
+
+    scalar_t med;
+    index_t n = trimmed.size();
+
+    if(n % 2)
+    {
+        med = trimmed[n/2];
+    }
+    else
+    {
+        med =
+            0.5 * (trimmed[n/2-1] + trimmed[n/2]);
+    }
+
+    return med / options_.scale_constant;
+}
+
+//------------------------------------------------------------
+// Tukey bisquare weights
+//------------------------------------------------------------
+Eigen::VectorXd RobustSolver::bisquare(
+    const Eigen::VectorXd& r,
+    scalar_t c
+) const
+{
+    Eigen::VectorXd w(r.size());
+
+    for(index_t i = 0; i < r.size(); i++)
+    {
+        scalar_t x = std::abs(r[i]);
+
+        if(x < c)
+        {
+            scalar_t t = r[i] / c;
+            w[i] = std::pow(1.0 - t*t, 2.0);
+        }
+        else
+        {
+            w[i] = 0.0;
+        }
+    }
+    return w;
+}
+
+bool RobustSolver::converged(
+    const Eigen::VectorXd& a,
+    const Eigen::VectorXd& b
+) const
+{
+    return ((a - b).array().abs().maxCoeff() < options_.tol);
+}
+
 
 Eigen::VectorXd RobustModel::predict(
     Eigen::Ref<const Eigen::MatrixXd> X
